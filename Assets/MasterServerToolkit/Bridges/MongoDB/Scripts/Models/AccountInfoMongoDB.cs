dec9cfc512c5af806695625f63bf0468ab48f0bd
Jasiel Macedo <jasielmacedo@gmail.com>
2021-02-23 01:30:45 -0300

    feat(mongodb): Add mongodb module bridge

---
+++ Assets/MasterServerToolkit/Bridges/MongoDB/Scripts/Models/AccountInfoMongoDB.cs
@@ -0,0 +1,92 @@
+#if (!UNITY_WEBGL && !UNITY_IOS) || UNITY_EDITOR
+using MasterServerToolkit.MasterServer;
+using System;
+using System.Collections.Generic;
+using MongoDB.Driver;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace MasterServerToolkit.Bridges.MongoDB
+{
+    public class AccountInfoMongoDB : IAccountInfoData
+    {
+        [BsonId]
+        public ObjectId _id { get; set; }
+        public string Id { get => _id.ToString(); set => _id = new ObjectId(value); }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Facebook { get; set; }
+        public string Token { get; set; }
+        public bool IsAdmin { get; set; }
+        public bool IsGuest { get; set; }
+        public bool IsEmailConfirmed { get; set; }
+        public Dictionary<string, string> Properties { get; set; }
+
+        public event Action<IAccountInfoData> OnChangedEvent;
+
+        public AccountInfoMongoDB()
+        {
+            Username = string.Empty;
+            Password = string.Empty;
+            Email = string.Empty;
+            PhoneNumber = string.Empty;
+            Facebook = string.Empty;
+            Token = string.Empty;
+            IsAdmin = false;
+            IsGuest = true;
+            IsEmailConfirmed = false;
+            Properties = new Dictionary<string, string>();
+        }
+
+        public void MarkAsDirty()
+        {
+            OnChangedEvent?.Invoke(this);
+        }
+
+        public bool HasToken()
+        {
+            return !string.IsNullOrEmpty(Token);
+        }
+
+        public bool IsTokenExpired()
+        {
+            Properties.TryGetValue(MstDictKeys.USER_AUTH_TOKEN_EXPIRES, out string expireTime);
+
+            if (!string.IsNullOrEmpty(expireTime))
+            {
+                long filetime = Convert.ToInt64(expireTime);
+                return DateTime.FromFileTime(filetime) <= DateTime.Now;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void SetToken(int tokenExpiresInDays)
+        {
+            Token = Mst.Helper.CreateRandomAlphanumericString(64);
+            Properties[MstDictKeys.USER_AUTH_TOKEN_EXPIRES] = DateTime.Now.AddDays(tokenExpiresInDays).ToFileTime().ToString();
+        }
+
+        public override string ToString()
+        {
+            var properties = new MstProperties();
+            properties.Set("Id", Id);
+            properties.Set("Username", Username);
+            properties.Set("Password", Password);
+            properties.Set("Email", Email);
+            properties.Set("PhoneNumber", PhoneNumber);
+            properties.Set("FacebookId", Facebook);
+            properties.Set("IsAdmin", IsAdmin);
+            properties.Set("IsGuest", IsGuest);
+            properties.Set("IsEmailConfirmed", IsEmailConfirmed);
+            properties.Append(Properties);
+
+            return properties.ToString();
+        }
+    }
+}
+#endif
---
+++ Assets/MasterServerToolkit/Bridges/MongoDB/Scripts/Models/PasswordResetDataMongoDB.cs
@@ -0,0 +1,18 @@
+﻿#if (!UNITY_WEBGL && !UNITY_IOS) || UNITY_EDITOR
+using MongoDB.Bson;
+using MasterServerToolkit.MasterServer;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace MasterServerToolkit.Bridges.MongoDB
+{
+    public class PasswordResetDataMongoDB : IPasswordResetData
+    {
+        [BsonId]
+        public ObjectId _id { get; set; }
+        public string Id { get => _id.ToString(); set => _id = new ObjectId(value); }
+
+        public string Email { get; set; }
+        public string Code { get; set; }
+    }
+}
+#endif
